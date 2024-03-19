5bdeed0837bc413e965a4042a78b36c51db072af
Inaki Villar <inaki.seri@gmail.com>
2024-03-19 15:54:14 -0700

    Adding example

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,12 +3,11 @@ plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
 }
-println(providers.environmentVariable("PR_CI_BUILD").isPresent)
+
 if (providers.environmentVariable("PR_CI_BUILD").isPresent) {
     rootProject.buildScan {
-        this.buildScanPublished {
-            println(this.buildScanUri.toString())
-            File("temp_build_scan_url.txt").writeText(this.buildScanUri.toString())
+        buildScanPublished {
+            File("temp_build_scan_url.txt").writeText(buildScanUri.toString())
         }
     }
 }
